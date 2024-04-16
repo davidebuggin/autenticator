ffbb28147428b01657d265af8bc48af260a4a6a0
davidebuggin <davide.buggin@gmail.com>
2024-04-16 16:48:06 +0200

    Create autenticator trait

--- src/main.rs
+++ src/main.rs
@@ -1,42 +1,69 @@
 use csv;
-use std::error::Error; 
-use std::fs::File; 
-use serde::Deserialize; 
-use promptly::prompt; 
-
-pub trait Autenticator {
-    fn login_check(&self) -> Result<bool, Box<dyn Error>>;
+use std::error::Error;
+use std::fs::File;
+// use std::intrinsics::mir::Return;
+use promptly::prompt;
+use serde::Deserialize;
+use std::string::String;
+trait Autenticator {
+    fn login_check(&self, user_id: &str, password: &str) -> Result<bool, Box<dyn Error>>;
 }
 
 #[derive(Debug, Deserialize)]
-pub struct User {
+struct User {
     #[serde(rename = "UserId")]
     user_id: String,
     #[serde(rename = "PasswordId")]
-    password_id: String,
+    password: String,
 }
 
-impl Autenticator for User{
-    fn login_check(&self) -> Result<bool, Box<dyn Error>>{
-        let file = File::open("UtentiRegistrati.csv")?;
-        let mut reader = csv::Reader::from_reader(file);
+struct Autentication {
+    file: File,
+}
 
-    for result in reader.deserialize() {
-        let user: User = result?;
-  
-        if user.user_id == self.user_id && user.password_id == self.password_id {
-            return Ok(true); 
+struct ToyAutentication {
+    safe_password: [String; 3],
+}
+
+impl Autenticator for Autentication {
+    fn login_check(&self, user_id: &str, password: &str) -> Result<bool, Box<dyn Error>> {
+        let mut reader = csv::Reader::from_reader(&self.file);
+
+        for result in reader.deserialize() {
+            let user: User = result?;
+
+            if user.user_id == user_id && user.password == password {
+                return Ok(true);
+            }
         }
-    }
 
-    Ok(false) 
+        Ok(false)
     }
 }
 
-fn main()-> Result<(), Box<dyn Error>>{
-    
-    let user = User { user_id : prompt("Inserisci l'ID utente")? , password_id : prompt("Inserisci la password")?};
-    match user.login_check() {
+impl Autenticator for ToyAutentication {
+    fn login_check(&self, user_id: &str, password: &str) -> Result<bool, Box<dyn Error>> {
+        for p in &self.safe_password {
+            if p.trim() == (password) {
+                return Ok(true);
+            }
+        }
+        Ok(false)
+    }
+}
+
+fn main() -> Result<(), Box<dyn Error>> {
+    let user = User {
+        user_id: prompt("Inserisci l'ID utente")?,
+        password: prompt("Inserisci la password")?,
+    };
+    let autenticator = Autentication {
+        file: File::open("UtentiRegistrati.csv")?,
+    };
+    let safe_password = ToyAutentication {
+        safe_password: ["1".to_string(), "2".to_string(), "3".to_string()],
+    };
+    match safe_password.login_check(&user.user_id, &user.password) {
         Ok(true) => println!("Accesso riuscito!"),
         Ok(false) => println!("Accesso fallito!"),
         Err(e) => eprintln!("Errore: {}", e),
@@ -44,9 +71,3 @@ fn main()-> Result<(), Box<dyn Error>>{
 
     Ok(())
 }
-
-
-
-
-
-
